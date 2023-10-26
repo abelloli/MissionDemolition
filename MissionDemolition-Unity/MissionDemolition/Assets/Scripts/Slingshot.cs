4cf82d55e17959589508ab79f3b9957027600c48
Austin Belloli <belloliaustin19@gmail.com>
2023-10-26 12:21:36 -0500

    1st
    First instance

---
+++ MissionDemolition-Unity/MissionDemolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+    public float velocityMult = 10f;
+
+    [Header("Dynamic")]
+
+    public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
+    private void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+        launchPos = launchPointTrans.position;
+    }
+    private void OnMouseEnter()
+    {
+       // print("Slingshot:OnMouseEnter()");
+       launchPoint.SetActive(true);
+    }
+    private void OnMouseExit()
+    {
+       // print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+    private void OnMouseDown()
+    {
+        aimingMode = true;
+        projectile = Instantiate(projectilePrefab) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+    private void Update()
+    {
+        if (!aimingMode) return;
+        Vector3 mousePos2D = Input.mousePosition;
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+    }
+}
